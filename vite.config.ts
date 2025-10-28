0c9fbf384a5a1557caa68f415959362587ac0dfd
jjxmonster <kuba.tarabasz@wp.pl>
2025-10-28 23:29:14 +0100

    chore: tanstack plugin for nitro

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import tailwindcss from "@tailwindcss/vite";
+import { nitroV2Plugin } from "@tanstack/nitro-v2-vite-plugin";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
-import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
 import viteTsConfigPaths from "vite-tsconfig-paths";
 
@@ -11,7 +11,7 @@ const config = defineConfig({
 		viteTsConfigPaths({
 			projects: ["./tsconfig.json"],
 		}),
-		nitro(),
+		nitroV2Plugin(),
 		tailwindcss(),
 		tanstackStart(),
 		viteReact(),
